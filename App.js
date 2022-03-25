204cddd47b6bd49005cfa123a396175429aeda20
Nazari-o <89309708+Nazari-o@users.noreply.github.com>
2022-03-25 21:26:40 +0100

    Update App.js

--- App.js
+++ App.js
@@ -4,5 +4,5 @@ tl.to('.list', {duration: 1, opacity: 1});
 gsap.from('.anim1', {delay: .5, opacity: 0, duration: 1, y: -50, stagger: .2});
 gsap.from('.left-side', {opacity: 0, delay: 1, duration: 1.5, y: -150, ease: 'bounce(1, 0.3)'});
 
-
+tl.from('footer', {delay: 1.3, opacity: 0});
 
