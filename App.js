66858e0324eec5a166fb67eaa80de2e42cc7edb8
Nazari-o <nazarec15235@email.cz>
2022-04-09 19:54:04 +0200

    update-1

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
-const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
+const tl = gsap.timeline({defaults: {ease: 'circ.out'}});
+
+// HEADER LOGO SHRINKING
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector('.logo').style.width = "200px";
+  } else {
+    document.querySelector(".logo").style.width = "300px";
+  }
+}
+
+
 
 tl.from('.list', {duration: 1, delay: .4, opacity: 0});
 gsap.from('.anim1', {delay: .5, opacity: 0, duration: 1, y: -50, stagger: .2});
 gsap.from('.left-side', {opacity: 0, delay: 1, duration: 1.5, y: -150, ease: 'bounce(1, 0.3)'});
 
-tl.from('footer', {delay: 1.3, opacity: 0})
+gsap.from('.img-anim', {delay: 1, x: -400, opacity: 0, duration: 1.5, stagger: .3, scrollTrigger: '.img-anim'});
+gsap.from('.anim2', {delay: 1, x: 400, opacity: 0, duration: .5, stagger: .3, scrollTrigger: '.anim2', ease: 'circ.out'});
+// gsap.from('.footer', {delay: 1, duration: 1, opacity: 0, scrollTrigger: '.footer'});
+
+gsap.from('.anim3', {delay: 1, opacity: 0, duration: 1, y: -200, stagger: .3, scrollTrigger: '.anim3'});
+gsap.from('.anim31', {delay: 1, opacity: 0, duration: 1, y: 200, stagger: .3, scrollTrigger: '.anim31'});
+
+
 
