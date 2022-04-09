19ba348ad13fdf076e7f77b6a57e2fbd20e7c003
Nazari-o <89309708+Nazari-o@users.noreply.github.com>
2022-04-09 21:54:22 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const tl = gsap.timeline({defaults: {ease: 'circ.out'}});
+// const tl = gsap.timeline({defaults: {ease: 'circ.out'}});
 
 // HEADER LOGO SHRINKING
 window.onscroll = function() {scrollFunction()};
@@ -13,16 +13,16 @@ function scrollFunction() {
 
 
 
-tl.from('.list', {duration: 1, delay: .4, opacity: 0});
-gsap.from('.anim1', {delay: .5, opacity: 0, duration: 1, y: -50, stagger: .2});
-gsap.from('.left-side', {opacity: 0, delay: 1, duration: 1.5, y: -150, ease: 'bounce(1, 0.3)'});
+// tl.from('.list', {duration: 1, delay: .4, opacity: 0});
+// gsap.from('.anim1', {delay: .5, opacity: 0, duration: 1, y: -50, stagger: .2});
+// gsap.from('.left-side', {opacity: 0, delay: 1, duration: 1.5, y: -150, ease: 'bounce(1, 0.3)'});
 
-gsap.from('.img-anim', {delay: 1, x: -400, opacity: 0, duration: 1.5, stagger: .3, scrollTrigger: '.img-anim'});
-gsap.from('.anim2', {delay: 1, x: 400, opacity: 0, duration: .5, stagger: .3, scrollTrigger: '.anim2', ease: 'circ.out'});
+// gsap.from('.img-anim', {delay: 1, x: -400, opacity: 0, duration: 1.5, stagger: .3, scrollTrigger: '.img-anim'});
+// gsap.from('.anim2', {delay: 1, x: 400, opacity: 0, duration: .5, stagger: .3, scrollTrigger: '.anim2', ease: 'circ.out'});
 // gsap.from('.footer', {delay: 1, duration: 1, opacity: 0, scrollTrigger: '.footer'});
 
-gsap.from('.anim3', {delay: 1, opacity: 0, duration: 1, y: -200, stagger: .3, scrollTrigger: '.anim3'});
-gsap.from('.anim31', {delay: 1, opacity: 0, duration: 1, y: 200, stagger: .3, scrollTrigger: '.anim31'});
+// gsap.from('.anim3', {delay: 1, opacity: 0, duration: 1, y: -200, stagger: .3, scrollTrigger: '.anim3'});
+// gsap.from('.anim31', {delay: 1, opacity: 0, duration: 1, y: 200, stagger: .3, scrollTrigger: '.anim31'});
 
 
 
