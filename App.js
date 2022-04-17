babe0071c6d36341407d595945102568d6f0d6c2
Nazari-o <nazarec15235@email.cz>
2022-04-17 19:22:28 +0200

    menu-update

--- App.js
+++ App.js
@@ -1,40 +1,21 @@
-// const tl = gsap.timeline({defaults: {ease: 'circ.out'}});
-
-// HEADER LOGO SHRINKING
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.querySelector('.logo').style.width = "200px";
-//   } else {
-//     document.querySelector(".logo").style.width = '300px';
-//   }
-// }
-
-
-
-// tl.from('.list', {duration: 1, delay: .4, opacity: 0});
-// gsap.from('.anim1', {delay: .5, opacity: 0, duration: 1, y: -50, stagger: .2});
-// gsap.from('.left-side', {opacity: 0, delay: 1, duration: 1.5, y: -150, ease: 'bounce(1, 0.3)'});
-
-// gsap.from('.img-anim', {delay: 1, x: -400, opacity: 0, duration: 1.5, stagger: .3, scrollTrigger: '.img-anim'});
-// gsap.from('.anim2', {delay: 1, x: 400, opacity: 0, duration: .5, stagger: .3, scrollTrigger: '.anim2', ease: 'circ.out'});
-// gsap.from('.footer', {delay: 1, duration: 1, opacity: 0, scrollTrigger: '.footer'});
-
-// gsap.from('.anim3', {delay: 1, opacity: 0, duration: 1, y: -200, stagger: .3, scrollTrigger: '.anim3'});
-// gsap.from('.anim31', {delay: 1, opacity: 0, duration: 1, y: 200, stagger: .3, scrollTrigger: '.anim31'});
-
+const body = document.querySelector('body');
 const burgerMenu = document.querySelector('.mobile-menu');
 const navbar = document.querySelector('.navbar');
-const linkList = document.querySelector('.list');
+const linkList = document.querySelector('ul');
+const header = document.querySelector('.header');
 
 burgerMenu.addEventListener('click', openMenu);
 
-// document.querySelector('.navbar').style.display = 'block';
 function openMenu() {
-  if (navbar.style.display === 'block') {
-    navbar.style.display = 'none';
+  if (navbar.style.display != 'block') {
+    navbar.style.display = 'block';
   } else {
-    navbar.style.display = 'block'
+    navbar.style.display = 'none';
+
   }
 }
+
+linkList.addEventListener('click', () => {
+  navbar.style.display = 'none';
+})
+
